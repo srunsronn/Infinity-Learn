2595cdd2450d95049a200339edad2b84e19bbaf3
unknown <sokda106@gmail.com>
2025-02-25 18:20:51 +0700

    change enrolled course and savecourse

--- routes/enrolledCourseRoute.js
+++ routes/enrolledCourseRoute.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { enrolledCourse, getAllEnrolledCourses } from "../controllers/api/v1/enrolledCourseController.js";
+import {
+  enrolledCourse,
+  getAllEnrolledCourses,
+} from "../controllers/api/v1/enrolledCourseController.js";
 import { authenticate } from "../middlewares/authMiddleware.js";
 import verifyRole from "../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
-router.get("/:id", authenticate, getAllEnrolledCourses);
+router.get("/:id", getAllEnrolledCourses);
 router.post("/new-enroll", authenticate, verifyRole("admin"), enrolledCourse);
 
 export default router;
--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -1,24 +1,44 @@
 import express from "express";
 
-import { createQuestion,getAllQuestion, getQuestionById , updateQuestion, deleteQuestion,getQuestionByQuizId,deleteAll} from "../controllers/api/v1/questionController.js";
+import {
+  createQuestion,
+  getAllQuestion,
+  getQuestionById,
+  updateQuestion,
+  deleteQuestion,
+  getQuestionByQuizId,
+  deleteAll,
+} from "../controllers/api/v1/questionController.js";
 import { authenticate } from "../middlewares/authMiddleware.js";
 import verifyRole from "../middlewares/roleMiddleware.js";
 
-
 const router = express.Router();
 
-router.route("/create-question").post(authenticate,verifyRole('admin','teacher'), createQuestion);
-router.route("/:id").get(authenticate,verifyRole('admin','teacher','student'), getQuestionById);
+router
+  .route("/create-question")
+  .post(authenticate, verifyRole("admin", "teacher"), createQuestion);
+router
+  .route("/:id")
+  .get(
+    authenticate,
+    verifyRole("admin", "teacher", "student"),
+    getQuestionById
+  );
 
+//admin
+router
+  .route("/get-all-questions")
+  .post(authenticate, verifyRole("admin", "teacher"), getAllQuestion);
+router
+  .route("/quiz/:quiz_id")
+  .post(authenticate, verifyRole("admin", "teacher"), getQuestionByQuizId);
+router
+  .route("/:id/update")
+  .put(authenticate, verifyRole("admin", "teacher"), updateQuestion);
+router.route("/deleteAll").delete(authenticate, verifyRole("admin"), deleteAll);
 
-//admin 
-router.route("/get-all-questions").post(authenticate, verifyRole("admin","teacher"), getAllQuestion);
-router.route("/quiz/:quiz_id").post(authenticate, verifyRole("admin","teacher"), getQuestionByQuizId);
-router.route("/:id/update").put(authenticate, verifyRole("admin","teacher"), updateQuestion);
-router.route('/deleteAll').delete(authenticate,verifyRole('admin'),deleteAll);
-
-router.route("/delete/:id").delete(authenticate,verifyRole("teacher", "admin"), deleteQuestion);
-
-
+router
+  .route("/delete/:id")
+  .delete(authenticate, verifyRole("teacher", "admin"), deleteQuestion);
 
 export default router;
